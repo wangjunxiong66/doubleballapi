e7855c5b1e12b689de113076ad8c9749e132ddd8
finup <123456>
2020-09-03 09:54:11 +0800

    2020-09-03

--- src/main/java/com/doubleball/controller/OperateBallController.java
+++ src/main/java/com/doubleball/controller/OperateBallController.java
@@ -2,10 +2,7 @@ package com.doubleball.controller;
 
 //import org.apache.commons.beanutils.locale.converters.ByteLocaleConverter;
 import org.joda.time.DateTime;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -18,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
 import java.sql.SQLOutput;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -34,78 +33,131 @@ public class OperateBallController {
 
 
     @RequestMapping(value = "/openbrowser",method = RequestMethod.GET)
-    public void openBrowserWithFirxfoxAndChrome(){
+    public void openBrowserWithFirxfoxAndChrome() {
 
-        String geckodriverPath = System.getProperty("user.dir")+"/src/main/resources/geckodriver-v0.24.0-macos";
+        String geckodriverPath = System.getProperty("user.dir") + "/src/main/resources/geckodriver-v0.24.0-macos";
 
         //  指定firefox的安装路径
         System.setProperty("webdriver.gecko.driver", geckodriverPath);
 
         WebDriver driver = new FirefoxDriver();
         // 在火狐浏览器打开map.baidu.com
-        driver.get("https://m.iqianjin.com/m/login/");
-        WebElement name = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div[1]/input"));
-        WebElement password = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div[2]/input"));
-        WebElement submit = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/button"));
-        name.sendKeys("wjx0001");
-        password.sendKeys("123456a");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        submit.click();
-
-//        driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
-//        String getWindowHandle = driver.getWindowHandle();
-//        System.out.println("getWindowHandle  is   "+getWindowHandle);
-//        System.out.println("getPagesource  is   "+driver.getPageSource());
-//        driver.navigate().to("http://www.hao123.com");
-//        url = driver.getCurrentUrl();
-//        System.out.println(url);
-//        driver.navigate().to("http://www.baidu.com");
-//        url = driver.getCurrentUrl();
-//        System.out.println(url);
-
-//        WebElement searchBox = driver.findElement(By.id("kw"));
-//        searchBox.sendKeys(Keys.chord(Keys.SHIFT+"abcde"));
-//        try {
-//            Thread.sleep(5000);
-//            System.out.println("等待5秒，准备提交=======");
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        WebElement sbumitBox = driver.findElement(By.id("su"));
-//        System.out.println("Value of the button is:"+sbumitBox.getAttribute("value"));
-//        System.out.println("text of the button is:"+sbumitBox.getText());
-//        System.out.println("tag name of the button is:"+sbumitBox.getTagName());
-//        System.out.println("CSS height of the button is:"+sbumitBox.getCssValue("height"));
-//        System.out.println("CSS of the button is:"+sbumitBox.getClass());
-//        System.out.println("location of the button is:"+sbumitBox.getLocation());
-//        System.out.println("size of the button is:"+sbumitBox.getSize());
-//
-//        sbumitBox.submit();
-//        try {
-//            Thread.sleep(5000);
-//            System.out.println("点击完毕=======");
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        driver.quit();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-//        driver.close();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        driver.get("http://www.baidu.com");
+        System.out.println("Go to url: " + driver.getCurrentUrl());
+        driver.navigate().to("http://www.cnblogs.com");
+        System.out.println("Navigate to url: " + driver.getCurrentUrl());
+        driver.navigate().refresh();
+        driver.navigate().back();
+        System.out.println("Back to url:  " + driver.getCurrentUrl());
+        driver.navigate().forward();
+        System.out.println("Forward to url: " + driver.getCurrentUrl());
+        driver.quit();
 
     }
 
+
+//====  以下是cookie的操作  ====  以下是cookie的操作  ========  以下是cookie的操作  ========  以下是cookie的操作  ========  以下是cookie的操作  ====
+//        String geckodriverPath = System.getProperty("user.dir")+"/src/main/resources/geckodriver-v0.24.0-macos";
+//
+//        //  指定firefox的安装路径
+//        System.setProperty("webdriver.gecko.driver", geckodriverPath);
+//
+//        WebDriver driver = new FirefoxDriver();
+//        // 在火狐浏览器打开map.baidu.com
+//        driver.get("https://m.iqianjin.com/m/login/");
+//        WebElement name = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div[1]/input"));
+//        WebElement password = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/div[2]/input"));
+//        WebElement submit = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[3]/button"));
+//        name.sendKeys("wjx0001");
+//        password.sendKeys("123456a");
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        submit.click();
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+////        driver.close();
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        File cookieFile = new File("/Users/finup/Downloads/zhihucookie.txt") ;
+//        try{
+//            System.out.println("开始写cookie");
+//            cookieFile.delete() ;
+//            cookieFile.createNewFile() ;
+//            FileWriter fileWriter = new FileWriter(cookieFile) ;
+//            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter) ;
+//            for ( Cookie cookie : driver.manage().getCookies()){
+//                bufferedWriter.write((cookie.getName() + ";" + cookie.getValue( ) + ";"
+//                        + cookie.getDomain( ) + ";" + cookie.getPath( ) + ";"
+//                        + cookie.getExpiry( ) + ";" + cookie.isSecure()));
+//                bufferedWriter.newLine();
+//            }
+//            bufferedWriter.flush();
+//            bufferedWriter.close();
+//
+//            System.out.println("cookie写完，请退出登录");
+//        }catch (Exception ex){
+//            ex. printStackTrace();
+//        }
+//
+//        try {
+//            System.out.println("开始退出登录");
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            System.out.println("开始读cookie");
+//            FileReader fr = new FileReader(cookieFile) ;
+//            BufferedReader bufferedReader = new BufferedReader(fr) ;
+//            String line;
+//            while ((line = bufferedReader.readLine())!=null) {
+//                StringTokenizer stringTokenizer =new StringTokenizer( line, ";");  // 构造一个用来解析 line 的 StringTokenizer 对象，并提供一个指定的分隔符。
+//                while ( stringTokenizer.hasMoreTokens()) {         // 返回是否还有分隔符
+//                    String name1 = stringTokenizer.nextToken();
+//                    String value = stringTokenizer.nextToken();
+//                    String domain= stringTokenizer.nextToken();
+//                    String path = stringTokenizer.nextToken();
+//                    Date expiry = null;
+//                    String dt;
+//                    if(!(dt = stringTokenizer.nextToken()).equals("null")) {
+//                        expiry = new Date(dt) ;
+//                    }
+//                    boolean isSecure =
+//                            new Boolean(stringTokenizer.nextToken()).booleanValue();
+//                    Cookie cookie = new Cookie(name1,value,domain,path,expiry,isSecure) ;
+//                    driver.manage().addCookie(cookie);
+//                }
+//            }
+//            System.out.println("cookie读完");
+//        }catch ( Exception ex){
+//            ex.printStackTrace();
+//        }
+//        try {
+//            System.out.println("开始停留5秒");
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("开始刷页面 https://m.iqianjin.com/m/user-center");
+//        driver.get("https://m.iqianjin.com/m/user-center");
+//        try {
+//            System.out.println("开始停留10秒");
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//====  以上是cookie的操作  ====  以上是cookie的操作  ====  以上是cookie的操作  ====  以上是cookie的操作  ====  以上是cookie的操作  ====  以上是cookie的操作
 
 }
